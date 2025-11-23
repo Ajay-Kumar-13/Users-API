3ab45ae644eb4b64654cd3581b852673b00022b3
Ajay-Kumar-13 <apulleti@gitam.in>
2025-11-23 23:49:46 +0530

    configure postsqlgre

--- src/main/java/com/crm/users/controller/AdminController.java
+++ src/main/java/com/crm/users/controller/AdminController.java
@@ -1,16 +1,29 @@
 package com.crm.users.controller;
 
+import com.crm.users.model.User;
+import com.crm.users.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/users")
 public class AdminController {
 
-    @GetMapping("/")
-    public String getAllUsers() {
-        return "This Endpoint will return all the users";
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping()
+    public Mono<User> getAllUsers() {
+        User user = new User();
+        user.setName("AJAY");
+        user.setMobile("9346072205");
+        return userRepository.save(user);
     }
 
 }
